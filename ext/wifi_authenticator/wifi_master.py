2919bd723f7473b088e781e5f8d72201bc84ff22
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-08-01 18:55:57 -0700

    Add probe/auth/assoc request events

--- ext/wifi_authenticator/wifi_master.py
+++ ext/wifi_authenticator/wifi_master.py
@@ -9,12 +9,12 @@ from pox.lib.util import str_to_bool
 import time
 
 import dpkt, binascii
+from pox.lib.revent import *
 
 log = core.getLogger()
 
-WLAN_TYPE_MGMT = 0
-WLAN_SUBTYPE_PROBE_REQUEST = 4
-WLAN_SUBTYPE_PROBE_RESPONSE = 5
+WIFI_MONITOR_PORT = 3 # monitor port where we expect mgmt packets from.
+
 
 PROBE_RESPONSE_STR = "\x00\x00\x1a\x00\x2f\x48\x00\x00\x93\x44\x1f\x90\x4e\x4e\x00\xd2\x10\x02\x6c\x09\xa0\x00\xdb\x01\x00\x00\x50\x00\x3a\x01\xc8\x3a\x35\xcf\xcc\x37\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x01\xc0\x37\xb0\xdb\x28\x05\x00\x00\x00\x00\x64\x00\x01\x04\x00\x06\x70\x69\x2d\x61\x70\x31\x01\x08\x82\x84\x8b\x96\x0c\x12\x18\x24\x03\x01\x01\x2a\x01\x06\x32\x04\x30\x48\x60\x6c\xdd\x18\x00\x50\xf2\x02\x01\x01\x00\x00\x03\xa4\x00\x00\x27\xa4\x00\x00\x42\x43\x5e\x00\x62\x32\x2f\x00\x28\xf3\xe0\x3a"
 
@@ -23,18 +23,58 @@ AUTH_REPLY_STR = "\x00\x00\x12\x00\x2e\x48\x00\x00\x00\x02\x6c\x09\xa0\x00\xd9\x
 ASSOC_REPLY_STR="\x00\x00\x12\x00\x2e\x48\x00\x00\x00\x02\x6c\x09\xa0\x00\xd7\x01\x00\x00\x10\x00\x3a\x01\xc8\x3a\x35\xcf\xcc\x37\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x01\x80\x3a\x01\x04\x00\x00\x01\xc0\x01\x08\x82\x84\x8b\x96\x0c\x12\x18\x24\x32\x04\x30\x48\x60\x6c\xdd\x18\x00\x50\xf2\x02\x01\x01\x00\x00\x03\xa4\x00\x00\x27\xa4\x00\x00\x42\x43\x5e\x00\x62\x32\x2f\x00"
 
 
+class ProbeRequest(Event):
+    '''Event raised by an AP when a probe request is received.
+    @dpid : the AP reporting the request
+    @src_addr : host's address
+    @snr: the snr of the packet
+    '''
+    def __init__(self, dpid, src_addr, snr):
+        Event.__init__(self)
+        self.dpid = dpid
+        self.src_addr = src_addr
+        self.snr = snr
+    
+class AuthRequest(Event):
+    '''Event raised by an AP when a probe request is received.
+    @dpid : the AP reporting the request
+    @src_addr : host's address
+    @snr: the snr of the packet
+    '''
+    def __init__(self, dpid, src_addr, snr):
+        Event.__init__(self)
+        self.dpid = dpid
+        self.src_addr = src_addr
+        self.snr = snr
 
-class WifiAuthenticateSwitch(object):
+class AssocRequest(Event):
+    '''Event raised by an AP when a probe request is received.
+    @dpid : the AP reporting the request
+    @src_addr : host's address
+    @snr: the snr of the packet
+    '''
+    def __init__(self, dpid, src_addr, snr):
+        Event.__init__(self)
+        self.dpid = dpid
+        self.src_addr = src_addr
+        self.snr = snr
+
+
+
+class WifiAuthenticateSwitch(EventMixin):
+    _eventMixin_events = set([ProbeRequest, AuthRequest, AssocRequest])
+    
     def __init__(self, connection, transparent):
+        EventMixin.__init__(self)
         self.connection = connection
         self.transparent = transparent
         
         connection.addListeners(self)
 
     def _handle_PacketIn(self, event):
-        if event.port != 3:
-            #print "not monitor port - ignore (port:%d)" % event.port
+        if event.port != WIFI_MONITOR_PORT:
             return
+
         packet = event.parsed
         rdtap = dpkt.radiotap.Radiotap(packet.raw)
         rd_len = rdtap.length >> 8
@@ -50,16 +90,15 @@ class WifiAuthenticateSwitch(object):
         print binascii.hexlify(packet.raw[rd_len:rd_len+8])
 
         if (ie.type == dpkt.ieee80211.MGMT_TYPE and ie.subtype == dpkt.ieee80211.M_PROBE_REQ):
-            print "sending probe response!!!"
-            self.send_packet_out(PROBE_RESPONSE_STR)
+            log.debug("Received probe request!!")
+            self.raiseEvent(ProbeRequest(event.dpid, ie.mgmt.src, 0))
 
         if (ie.type == dpkt.ieee80211.MGMT_TYPE and ie.subtype == dpkt.ieee80211.M_AUTH):
-            print "sending auth reply!!!"
-            self.send_packet_out(AUTH_REPLY_STR)
+            self.raiseEvent(AuthRequest(event.dpid, ie.mgmt.src, 0))
+            #self.send_packet_out(AUTH_REPLY_STR)
             
         if (ie.type == dpkt.ieee80211.MGMT_TYPE and ie.subtype == dpkt.ieee80211.M_ASSOC_REQ):
-            print "sending assoc reply!!!"
-            self.send_packet_out(ASSOC_REPLY_STR)
+            self.raiseEvent(AssocRequest(event.dpid, ie.mgmt.src, 0))
 
         if (ie.type == 0 and ie.subtype != 8):
             print "Received %x from %s" % (ie.subtype, binascii.hexlify(ie.mgmt.src))
@@ -78,10 +117,22 @@ class WifiAuthenticator(object):
     def __init__(self, transparent):
         core.openflow.addListeners(self)
         self.transparent = transparent
+        self.aps = {}
 
     def _handle_ConnectionUp(self, event):
         log.debug("Connection %s" % (event.connection))
-        WifiAuthenticateSwitch(event.connection, self.transparent)
+        wifi_ap = WifiAuthenticateSwitch(event.connection, self.transparent)
+        wifi_ap.addListeners(self)
+        self.aps[event.dpid] = wifi_ap
+
+    def _handle_ProbeRequest(self, event):
+        log.debug("Got a probe request event from %s!!" % dpid_to_str(event.dpid))
+
+    def _handle_AuthRequest(self, event):
+        log.debug("Got an auth request event from %s!!" % dpid_to_str(event.dpid))
+
+    def _handle_AssocRequest(self, event):
+        log.debug("Got an assoc request event from %s!!" % dpid_to_str(event.dpid))
 
 def launch( transparent=False):
     core.registerNew(WifiAuthenticator, str_to_bool(transparent))
