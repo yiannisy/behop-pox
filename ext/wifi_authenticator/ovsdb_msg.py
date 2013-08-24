4ef69a5b8abaf3a6c1f70a1a9e27eb008343d7ca
Yiannis Yiakoumis <yiannisy@stanford.edu>
2013-08-23 18:38:01 -0700

    Get DPID from OVSDB Server. Add Station.

--- ext/wifi_authenticator/ovsdb_msg.py
+++ ext/wifi_authenticator/ovsdb_msg.py
@@ -7,16 +7,63 @@ from pox.messenger import *
 
 log = core.getLogger()
 
+G_RATES = [1,2,6,11,12,18,24,36,48,54]
+G_RATES_LEN = len(G_RATES)
 
-class OvsDBBot(ChannelBot):
+GET_DPID = {"method":"transact", "params":["Open_vSwitch", {"op":"select","table":"Bridge","where":[],"columns":["datapath_id"]}], "id":1}
+
+ADD_STATION = {"method":"transact","params":["Open_vSwitch",{"op":"insert","table":"WifiSta","row":{"addr":None,"vbssid":None}}],"id":1}
+
+class OvsDBBot(ChannelBot, EventMixin):
+    def __init__(self, channel, nexus = None, weak = False, extra = {}):
+        ChannelBot.__init__(self, channel, nexus, weak, extra)
+        self.connections = {}
+        self.listenTo(core.WifiAuthenticator)
+        log.debug("initialized OVSDBBot")
+
+    def _handle_ChannelCreate(self, event):
+        log.debug("Channel Created!")
+
+    def _handle_ChannelJoin(self, event):
+        log.debug("connection joined")
 
     def _handle_MessageReceived(self, event, msg):
-        if msg['method'] == 'echo':
+        log.debug(msg)
+        if msg.has_key("method") and msg['method'] == 'echo':
             self.send_echo_reply(event.con, msg)
+            if event.con not in self.connections.values():
+                # ask the DPID from ovsdb - this should happen on connection...
+                self.send_dpid_request(event.con)
+        # hacky way to get the DPID
+        elif msg.has_key("result"):
+            try:
+                dpid = int(msg["result"][0]["rows"][0]["datapath_id"],16)
+                self.connections[dpid] = event.con
+                log.debug("Appending OVSDB connection for %x" % dpid)
+            except KeyError:
+                pass
 
     def send_echo_reply(self, con, msg):
         con.send({"result":msg['params'],"error":None, 'id':msg['id']})
 
+    def send_dpid_request(self, con):
+        con.send(GET_DPID)
+
+    def _handle_AddStation(self, event):
+        log.debug("Received add station event")
+        log.debug(event)
+        add_json = ADD_STATION.copy()
+        add_json["params"][1]["row"] = {"addr":event.src_addr, "vbssid": event.vbssid}
+        log.debug("Adding Station %s to %d" % (event.src_addr, event.dpid))
+        if self.connections.has_key(event.dpid):
+            con = self.connections[event.dpid]
+            con.send(add_json)
+            log.debug(add_json)
+        else:
+            log.debug("key not found...")
+            for key in self.connections.keys():
+                log.debug("DPID : %x" % key)
+        
 class OvsDBManager(object):
     def __init__(self):
         core.listen_to_dependencies(self)
