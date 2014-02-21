268f953605b40e8ec8969b6436beceae17a979c8
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-02-21 15:34:27 -0800

    Catch expection when loading sta-whitelist from the DB.

--- ext/wifi_authenticator/util.py
+++ ext/wifi_authenticator/util.py
@@ -13,14 +13,17 @@ def get_lan_from_wan_2(wan_addr):
 
 
 def load_sta_whitelist_from_db(db=BEHOP_DB_FILE):
-    
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-
-    c.execute("select addr,dpid from mac_user_dpid where opt_out == \"0\"")    
-    conn.commit()
-    w_stas = {int(x[0],16):int(x[1],16) for x in c.fetchall()}
-    conn.close()
+    w_stas = {}
+    try:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        
+        c.execute("select addr,dpid from mac_user_dpid where opt_out == \"0\"")    
+        conn.commit()
+        w_stas = {int(x[0],16):int(x[1],16) for x in c.fetchall()}
+        conn.close()
+    except:
+        print "Cannot load sta whitelist database"
     return w_stas
 
 def load_sta_whitelist_from_file(fname=WHITELIST_FNAME):
